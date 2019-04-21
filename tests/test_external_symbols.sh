52eb15578135ebad528433ad03490539d37209de
Matthias Räncker <theonetruecamper@gmx.de>
2019-04-21 13:35:55 +0200

    tests/test_external_symbols.sh : filther thunks in x86 pic builds
    Signed-off-by: Matthias Räncker <theonetruecamper@gmx.de>

--- tests/test_external_symbols.sh
+++ tests/test_external_symbols.sh
@@ -4,7 +4,7 @@
 
 set -eu${BASH+o pipefail}
 
-if nm -go --defined-only ../src/.libs/libkvazaar.a | grep -v ' kvz_'; then
+if nm -go --defined-only ../src/.libs/libkvazaar.a | grep -v ' kvz_' | grep -v ' __x86.get_pc_thunk.'; then
     printf '%s\n' 'Only symbols prefixed with "kvz_" should be exported from libkvazaar.'
     false
 fi
